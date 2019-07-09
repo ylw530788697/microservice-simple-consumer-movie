035e4d8a4aff6298c48bd02682319f0d41a72c98
v_liwenyang <530788697@qq.com>
2019-07-09 10:57:30 +0800

    eureka

--- src/main/java/com/itmuch/ConsumerMovieApplication.java
+++ src/main/java/com/itmuch/ConsumerMovieApplication.java
@@ -2,6 +2,7 @@ package com.itmuch;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
@@ -11,6 +12,7 @@ import org.springframework.web.client.RestTemplate;
  * @date 2019/06/17 22:08
  */
 @SpringBootApplication
+@EnableEurekaClient
 public class ConsumerMovieApplication {
     @Bean
     public RestTemplate restTemplate(){
